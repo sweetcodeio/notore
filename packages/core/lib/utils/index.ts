c96d9860a1ca4387420049b921445875b86bfc8f
Pablo Vinícius <dev.avenger.suicide@gmail.com>
2020-03-22 16:43:01 -0300

    core/utils: parseError method fix

--- packages/core/lib/utils/index.ts
+++ packages/core/lib/utils/index.ts
@@ -2,10 +2,8 @@ import { IValidField } from '../types';
 
 import { ERRORS } from './Constants';
 
-export function parseError(error: string, options?: IValidField): string {
-  if (options) {
-    if (options.complete) return ERRORS.complete(error);
-  }
+export function parseError(error: string, options: IValidField): string {
+  if (options.complete) return ERRORS.complete(error);
 
   return error;
 }
